beef08929174cedcdd5416c81bd130e1e69992f7
Swati Singh <swatisingh.it2023@nsec.ac.in>
2026-02-20 00:18:26 +0530

    optimal 4 sum done

--- Arrays/Hard/4Sum_optimal.cpp
+++ Arrays/Hard/4Sum_optimal.cpp
@@ -7,9 +7,38 @@ using namespace std;
 
 vector<vector<int>> four_sum(vector<int>& nums, int target) {
     int n = nums.size();
-    
+   vector<vector<int>>res;
+   sort(nums.begin(),nums.end());
 
-    return vector<vector<int>>(st.begin(), st.end());
+   for(int i=0;i<n;i++){
+    if(i>0 && nums[i]==nums[i-1]) continue;
+    for(int j =i+1;j<n;j++){
+        if(j>i+1 && nums[j]==nums[j-1]) continue;
+
+        int k =j+1, l=n-1 ;
+        while(k<l){
+            int sum = nums[i]+nums[j]+nums[k]+nums[l];
+            if(sum<target){
+            k++;
+            }
+            else if(sum>target){
+                l--;
+            }
+            else{
+                res.push_back({nums[i],nums[j],nums[k],nums[l]});
+                k++;
+                l--;
+
+                while(k<l && nums[k]==nums[k-1]) k++;
+                while(k<l && nums[l]== nums[l-1])  l--;
+
+            }
+        }
+    }
+   
+   }
+
+    return res;
 }
 
 int main() {
