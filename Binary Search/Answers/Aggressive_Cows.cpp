2d0a14d44456d7649d7b8874e19621f76e18b692
Swati Singh <swatisingh.it2023@nsec.ac.in>
2026-02-26 01:16:07 +0530

    omit

--- Binary Search/Answers/Aggressive_Cows.cpp
+++ Binary Search/Answers/Aggressive_Cows.cpp
@@ -14,4 +14,22 @@ Result:
 Explanation:
  The maximum possible minimum distance between any two cows will be 5 when 2 cows are placed at positions {1, 6}.  */
 
- 
+ int Aggressive_cows(vector<int>&nums,int k){
+    int n = nums.size();
+    int low =0;
+    int high =n-1;
+    while(low<=high){
+        int mid = (low+high)/2;
+    }
+ }
+
+ int main(){
+    int n , k;
+    cin>>n>>k;
+    vector<int>nums(n);
+    for(int i =0;i<n;i++){
+        cin>>nums[i];
+    }
+    cout<<Aggressive_cows(nums,k)<<" ";
+    return 0;
+ }
