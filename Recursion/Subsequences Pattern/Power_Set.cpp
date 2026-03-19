c071d5009aed6dad84b734e7ad6712b15a6ce9f3
Swati Singh <swatisingh.it2023@nsec.ac.in>
2026-03-19 15:35:10 +0530

    power set solved

---
+++ Recursion/Subsequences Pattern/Power_Set.cpp
@@ -0,0 +1,31 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+void helper(string &s , int index,string &curr, vector<string>&res ){
+    if(index==s.size()){
+        res.push_back(curr);
+        return;
+    }
+
+    helper(s,index+1,curr,res);
+    curr.push_back(s[index]);
+    helper(s,index+1, curr,res);
+    curr.pop_back();
+}
+vector<string>powerset(string s){
+    vector<string>res;
+    string curr = "";
+    helper(s,0,curr,res);
+    return res;
+}
+
+int main(){
+    string s;
+    cin>>s;
+    vector<string>ans = powerset(s);
+    for(int i =0;i<ans.size();i++){
+       cout << "\"" << ans[i] << "\"" << endl;
+    }
+    return 0;
+}
