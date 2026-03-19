301b6a92c52ad3abdf2fb3e5aa39e89db02ba92d
Swati Singh <swatisingh.it2023@nsec.ac.in>
2026-03-20 00:04:41 +0530

    subset 2 solved

---
+++ Recursion/Subsequences Pattern/Subset-2.cpp
@@ -0,0 +1,42 @@
+#include<iostream>
+#include<vector>
+#include <algorithm>
+using namespace std;
+void helper(vector<int>&nums, int index , int sum , vector<int>&curr , vector<vector<int>>&res){
+    if(index==nums.size()){
+        res.push_back(curr);
+return;
+    }
+    helper(nums, index+1, sum , curr, res);
+    curr.push_back(nums[index]);
+    helper(nums, index+1, sum+nums[index] , curr, res);
+curr.pop_back();
+}
+ 
+vector<vector<int>>subset2(vector<int>&nums){
+    vector<vector<int>>res;
+    vector<int>curr;
+
+    helper(nums,0,0,curr,res);
+    sort(nums.begin(),nums.end());
+    return res;
+}
+
+int main(){
+    int n ;
+    cin>>n;
+    vector<int>nums(n);
+    for(int i =0;i<n;i++){
+        cin>>nums[i];
+    }
+    vector<vector<int>>ans= subset2(nums);
+    for(int i =0;i<ans.size();i++){
+        cout<<"[";
+        for(int j =0;j<ans[i].size();j++){
+            cout<<ans[i][j]<<" ";
+
+        }
+        cout<<"]\n";
+    }
+    return 0;
+}
